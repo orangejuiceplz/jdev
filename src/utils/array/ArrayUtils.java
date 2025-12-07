236c181f408fde5e89bdb14678c0025d24d53b10
LL-LQ-84i <155986030+orangejuiceplz@users.noreply.github.com>
2025-12-07 10:34:04 -0500

    add arr utils

---
+++ src/utils/array/ArrayUtils.java
@@ -0,0 +1,19 @@
+package utils.array;
+
+public class ArrayUtils {
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+
+    public static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1])
+                return false;
+        }
+        return true;
+    }
+
+}
